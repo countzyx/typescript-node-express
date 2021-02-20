6841aca9d15835fd07e92286d6174551ca170017
Pedro R. Jiménez <countzyx@gmail.com>
2021-02-19 20:03:12 -0500

    Added Todo Update

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from 'express';
+import { RequestHandler, response } from 'express';
 import * as _ from 'lodash';
 import { Todo } from '../models/todo';
 
@@ -13,7 +13,21 @@ export const createTodo: RequestHandler = (req, res, next) => {
   next();
 };
 
-export const getTodos: RequestHandler = (req, res, next) => {
+export const getTodos: RequestHandler = (_0, res, next) => {
   res.json({ todos: TODOS });
   next();
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+  const todoIndex = TODOS.findIndex((t) => t.id === todoId);
+
+  if (todoIndex === -1) {
+    res.status(404).json({ message: `ID ${todoId} not found` });
+  }
+
+  TODOS[todoIndex] = new Todo(todoId, updatedText);
+
+  res.json({ message: `Todo ID ${todoId} updated`, todo: TODOS[todoIndex] });
 };
